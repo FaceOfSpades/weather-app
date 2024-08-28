9851fb2246dcf9d8cc6bdf276ffdb460e2c4ff65
Casey <141671253+FaceOfSpades@users.noreply.github.com>
2024-08-28 14:50:40 -0400

    Complete weather app
    add correct styling and add giphy search that
    matches current weather conditions

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ async function getTemp(search) {
     try {
     let weather = await getWeather(search);
     Display(weather);
+    generateBackground(weather);
     } catch(error) {
         console.log(error);
         throw new Error("stop");
@@ -27,22 +28,58 @@ const getSearch = function () {
 }();
 
 function Display(weather) {
-    console.log(weather);
+    clearDisplay();
     const display = document.querySelector(".display-container");
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const d = new Date();
     let count = d.getDay();
+    let dayCount = 0;
     weekday.forEach((e) => {
         const div = document.createElement("div");
         const span = document.createElement("span");
+        const tempHigh = document.createElement("div");
+        const tempLow = document.createElement("div");
+        div.classList.add("tiles");
+        tempHigh.classList.add("high");
+        tempLow.classList.add("low");
+        tempHigh.textContent = Math.round(weather.days[dayCount].tempmax);
+        tempLow.textContent = Math.round(weather.days[dayCount].tempmin);
         display.appendChild(div);
         let day = weekday[count];
         span.textContent = day;
         div.appendChild(span);
+        div.appendChild(tempHigh);
+        div.appendChild(tempLow);
         if (count == 6) {
             count = 0
         } else {
             count++;
         };
-    });    
+        dayCount++;
+    });
+    const description = document.createElement("div");
+    description.classList.add("description");
+    description.textContent = weather.description;
+    display.appendChild(description);
+}
+
+function clearDisplay() {
+    const display = document.querySelector(".display-container");
+    while (display.firstChild) {
+        display.removeChild(display.lastChild);
+    };
+}
+
+async function generateBackground(weather) {
+    let search = weather.currentConditions.conditions;
+    let gif = await getGif(search);
+    const img = document.querySelector("img");
+    img.src = gif.data.images.original.url;
+    img.style.display = "block";
+}
+
+async function getGif(search) {
+    let response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=ZVP1eoUUDXmIeO2EqrQW3qk2BhigaK92&s=weather ' + search, {mode: 'cors'})
+    let result = await response.json();
+    return result;
 }
